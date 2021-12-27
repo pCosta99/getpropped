a927ec1c990271b8878e334d303be34b72fe9eee
pCosta99 <costapedro.a1999@gmail.com>
2021-12-27 23:32:52 +0000

    jesus this was long

---
+++ app/app/javascript/controllers/web3_controller.js
@@ -0,0 +1,145 @@
+import { Controller } from "@hotwired/stimulus"
+import { prpABI } from 'prp_abi'
+
+export default class extends Controller {
+  static targets = [
+    // Connection related
+    "connectedDiv", "actionButton", "disconnectedDiv", // "address", "balance", "prpBalance",
+    // Props related
+    "proppedDiv"
+  ];
+
+  contractWithSigner = null;
+
+  async connect() {
+    ethereum.on('chainChanged', (chainId) => {
+      // Force a reload
+      window.location.reload();
+    });
+
+    ethereum.on('accountsChanged', (accounts) => {
+      // Force a reload
+      window.location.reload();
+    });
+
+    // Check if we are on fantom mainnet (and connected with any account)
+    const accounts = await ethereum.request({method: 'eth_accounts'});
+    if(ethereum.isConnected() && accounts.length > 0 && ethereum.networkVersion == "250"){
+      await this.handleConnected();
+    } else if (ethereum.isConnected() && accounts.length > 0) {
+      this.button_switchToFantom();
+      this.connectedDivTarget.style.visibility = "hidden";
+    } else {
+      this.button_connectToMM();
+      this.connectedDivTarget.style.visibility = "hidden";
+    }
+  }
+
+  async handleConnected(){
+    this.disconnectedDivTarget.style.visibility = "hidden";
+
+    const tokenContractAddress = "0x9D35FFf07fA008399249AC58470e39E14f1f27B2";
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+
+    const contract = new ethers.Contract(tokenContractAddress, prpABI, provider);
+    const address = ethereum.selectedAddress;
+
+    //const balance = await provider.getBalance(address);
+    //const prpBalance = await contract.balanceOf(address);
+    this.contractWithSigner = await contract.connect(signer);
+
+    //this.addressTarget.textContent = address;
+    //this.balanceTarget.textContent = ethers.utils.formatEther(balance);
+    //this.prpBalanceTarget.textContent = ethers.utils.formatUnits(prpBalance, 0);
+
+    await this.handleProps(contract, signer);
+  }
+
+  greet(event){
+    event.preventDefault();
+    console.log("abc");
+  }
+
+  button_connectToMM(){
+    this.actionButtonTarget.dataset["action"] = "web3#connectToMM"
+    this.actionButtonTarget.textContent = "Connect to Metamask"
+    this.actionButtonTarget.style.visibility = "visible"
+  }
+
+  button_switchToFantom(){
+    this.actionButtonTarget.dataset["action"] = "web3#switchToFantom"
+    this.actionButtonTarget.textContent = "Switch to Fantom"
+    this.actionButtonTarget.style.visibility = "visible"
+  }
+
+  button_getPropped(){
+    this.actionButtonTarget.dataset["action"] = "web3#getPropped"
+    this.actionButtonTarget.textContent = "Get propped!"
+    this.actionButtonTarget.style.visibility = "visible"
+  }
+
+  button_connectedToFantom() {
+    this.actionButtonTarget.dataset["action"] = ""
+    this.actionButtonTarget.textContent = "Connected to Fantom"
+    this.actionButtonTarget.style.visibility = "hidden"
+  }
+
+  async handleProps(contract, signer){
+    const hasBeenPropped = await this.contractWithSigner.wasPropped();
+
+    if(hasBeenPropped){
+      this.button_connectedToFantom()
+      this.alreadyPropped()
+    } else {
+      this.button_getPropped();
+    }
+  }
+
+  alreadyPropped(){
+    this.proppedDivTarget.style.visibility = "visible";
+  }
+
+  async getPropped(event){
+    event.preventDefault();
+    const actionButtonTarget = this.actionButtonTarget;
+    const proppedDivTarget = this.proppedDivTarget;
+    await this.contractWithSigner.getPropped()
+          .then(function(resp){
+            actionButtonTarget.style.visibility = "hidden";
+            proppedDivTarget.style.visibility = "visible";
+            console.log("welcome m8");
+          }).catch(function(error){
+            console.log(error);
+          });
+  }
+
+  connectToMM(event){
+    event.preventDefault();
+    ethereum.request({ method: 'eth_requestAccounts' });
+  }
+
+  switchToFantom(event){
+    event.preventDefault();
+    ethereum.request({
+      method: 'wallet_switchEthereumChain',
+      params: [{ chainId: '0x250' }]
+    }).then(function(value){
+      // Reload the window to display everything properly
+      window.location.reload();
+    }).catch(function(error){
+      if(error.code == 4902){
+        // Trigger a network switch
+        ethereum.request({
+          method: 'wallet_addEthereumChain',
+          params: [ {
+              chainId: '0xfa',
+              rpcUrls: ['https://rpc.ftm.tools/'],
+            } ]
+        }).catch(function(error){
+          console.log(error);
+        });
+      }
+    });
+  }
+}
